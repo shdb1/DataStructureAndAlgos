12251230c5132be4d7acca73a70e6cc307d3a470
Mohammad Shadab <shadab2266@gmail.com>
2018-05-21 20:50:10 +0530

    solution

---
+++ src/com/shadab/ds/challenges/print-the-elements-of-a-linked-list/solution.java
@@ -0,0 +1,26 @@
+/*
+  Print elements of a linked list on console 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+
+// This is a "method-only" submission. 
+// You only need to complete this method. 
+    
+void Print(Node head) {
+    if(head==null)
+        return;
+    while(head!=null){
+        System.out.println(head.data);
+        if(head.next!=null)
+         head=head.next;
+        else
+            return;
+    }
+  
+  
+}
